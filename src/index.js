b54969775e1f1ea518cc0a797aefb5c92df5a4a4
Nicholas Raynes <nicholas.r.raynes@gmail.com>
2021-07-28 11:37:42 -0600

    refectored to use useContext

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ let arrProm = [];
 for (let i=1;i <= 151;i++) {
   arrProm.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
     .then((data) => data.json())
-    .then((response) => response))
+    .then((response) => {
+      if (response) {
+        return response;
+      }}))
 }
 Promise.all(arrProm)
   .then((res) => {ReactDOM.render(
